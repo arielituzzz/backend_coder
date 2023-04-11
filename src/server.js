b56ae0021c6254863acca9d38f4626c069fb8435
Ariel Suarez <suarezariel86@gmail.com>
2023-04-11 09:30:42 -0300

    finalizando desafio 4

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 import { resolve } from 'path';
-import {Server} from 'socket.io'
-import ProductManager from "./models/ProductManager.js"
+import {Server} from 'socket.io';
+import ProductManager from "./models/ProductManager.js";
 
 void(async() =>
 	{
@@ -27,45 +27,85 @@ void(async() =>
 			app.set('view engine', 'handlebars');
 			app.set('views', viewsPath);
 
-			
-			//app.use("/realTimeProducts", productsRouter);
-
 			const httpServer = app.listen(PORT, () => {console.log(`SERVER READY: Server listen on port ${PORT}`)});
 
 			const socketServer = new Server(httpServer);
-			
-			socketServer.on("connection", socket => {
 
-				console.log("Nuevo cliente conectado!");
+			socketServer.on("connection", async socket => {
 
-			socket.on("message", (data) => console.log(data));
+				console.log("Client on-line");
 
+				socket.emit("listProducts", await productManager.getProducts());	
+
+				socket.on("createProduct", async (data) => {
+
+					try
+					{
+						if(!data.thumbnail) {
+							data.thumbnail = null;
+							await productManager.addProduct(data);
+						}
+						socketServer.emit("liveProducts", await productManager.getProducts());
+
+					}
+					catch(e)
+					{
+						console.log(e);	
+					}
+
+
+				});
+
+				socket.on("deleteProduct", async (data) => {
+
+					try
+					{
+						await productManager.deleteProduct(data);
+
+						socketServer.emit("liveProducts", await productManager.getProducts());
+					}
+					catch(err)
+					{
+						console.log(err);
+					}
+
+
+
+				});
+
+			socket.on("disconnect", () => {
+
+				console.log("Client off-line");
+
+			});
 
 			});
 
 
-		//ENLISTO TODOS LOS PRODUCTOS (EN TIEMPO REAL)
-		app.get("/realTimeProducts", async (req, res) => {
-			
-			try
-			{
-			
-				// Traigo los productos del archiv    o Products.json
-			  const products = await productManager.getProducts();
-			
-			  res.render("realTimeProducts", {products:products})
-			}
-			catch(err) 
-			{
-			
-				res.status(404).json({status: "Error", message: err.message});
-			  
-			};
-			
-		});
 
+			//ENLISTO TODOS LOS PRODUCTOS (EN TIEMPO REAL)
+			app.get("/realTimeProducts", async (req, res) => {
 
+				res.render("realTimeProducts")
 
+			});
+
+			//ENLISTO TODOS LOS PRODUCTOS (ESTATICA)
+			app.get("/", async (req, res) => {
+
+				try
+				{
+					//Traigo los productos del archiv    o Products.json
+					const products = await productManager.getProducts();
+
+					res.render("home", {products:products})
+				}
+				catch(err) 
+				{
+					res.status(404).json({status: "Error", message: err.message});
+				};
+
+			});
 		}
 		catch (e)
 		{
